0a85b9699a5d46f285d5f2fc90c3b716020734cb
Ravi-Prakash-Maurya <161294016+Ravi-Prakash-Maurya@users.noreply.github.com>
2024-11-09 09:28:25 +0530

    Time: 0 ms (100%), Space: 8.5 MB (82.17%) - LeetHub

---
+++ 3133-minimum-array-end/3133-minimum-array-end.cpp
@@ -0,0 +1,8 @@
+class Solution {
+public:
+    long long minEnd(int n, int x) {
+        bitset<64> X(x), N(n - 1), z = 0;
+        for(int i = 0, j = 0; i < 56; i++) z[i]= (X[i]) ? 1 : N[j++];
+        return z.to_ullong();
+    }
+};
