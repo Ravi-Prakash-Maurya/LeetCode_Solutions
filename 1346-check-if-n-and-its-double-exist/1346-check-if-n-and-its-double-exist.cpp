3029e5ae7e10697e5ea30a62db4eef805c84e41a
Ravi-Prakash-Maurya <161294016+Ravi-Prakash-Maurya@users.noreply.github.com>
2024-12-01 17:10:00 +0530

    Time: 4 ms (16.1%), Space: 13.5 MB (17.49%) - LeetHub

--- 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.cpp
+++ 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.cpp
@@ -5,10 +5,10 @@ public:
         for(int i = 0; i < arr.size(); i++) {
             int x = arr[i] * 2;
             if (x == 0) {
-                if (count(arr.begin(), arr.end(), 0) > 1)
+                if(count(arr.begin(), arr.end(), 0) > 1)
                     return true;
             } else {
-                if (find(arr.begin(), arr.end(), x) != arr.end())
+                if(find(arr.begin(), arr.end(), x) != arr.end())
                     return true;
             }
         }
